8de5cb90d66c16adf5be5c0614f73f63523ea0d5
alampobon34 <alampobon34@gmail.com>
2024-03-04 16:24:59 +0600

    changed the backend api path

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -14,11 +14,11 @@ interface ApiResponseWithPagination<T> {
   to: number;
 }
 
-const BACKEND_API_URL = 'http://localhost:3000/api'
+// const BACKEND_API_URL = 'http://localhost:3000/api';
+const BACKEND_API_URL = 'https://booking-app-p97n89x4x-alampobon34.vercel.app/api';
 
 const api = axios.create({
   baseURL: BACKEND_API_URL,
-  headers: { Authorization: `Bearer ynYF3oue23nVqpgq1bXy77OL9iXA` }
 });
 
 
